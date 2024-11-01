c78ee731a1f861c82afafc5c03b06d6e08a2346a
Vinicius Plack Wecker <viniciuswecker@ienh.com.br>
2024-11-01 00:29:33 -0300

    Ajustes SideBar e DefaultLayout

--- frontend/app/src/components/Sidebar/index.jsx
+++ frontend/app/src/components/Sidebar/index.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import './index.css'; // Importar estilos
+import React, { useState, useEffect } from 'react';
+import { useLocation, NavLink } from 'react-router-dom';
+import './index.css';
 
 import Icon from '@mui/material/Icon';
-
-//Icones Fa
 import { FaEdit } from "react-icons/fa";
 import { PiPulseBold } from "react-icons/pi";
-
-//Icones de Material UI
 import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
@@ -16,6 +12,22 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
 
 const SideMenu = () => {
+  const location = useLocation();
+  const [activePath, setActivePath] = useState(location.pathname);
+
+  useEffect(() => {
+    setActivePath(location.pathname);
+  }, [location.pathname]);
+
+  const menuItems = [
+    { path: '/dashboard', label: '     Dashboard', icon: <PiPulseBold /> },
+    { path: '/', label: '     Equipamentos', icon: <Inventory2OutlinedIcon /> },
+    { path: '/patrimonios', label: '     Patrimônios', icon: <Inventory2OutlinedIcon /> },
+    { path: '/emprestimos', label: '     Empréstimos', icon: <Inventory2OutlinedIcon /> },
+    { path: '/pacientes', label: '     Pacientes', icon: <SupervisedUserCircleIcon /> },
+    { path: '/relatorios', label: '     Relatórios', icon: <TrendingUpIcon /> },
+    { path: '/configuracoes', label: '     Configurações', icon: <SettingsOutlinedIcon /> },
+  ];
 
   return (
     <div className="container-sidebar">
@@ -25,44 +37,27 @@ const SideMenu = () => {
       <div className="content-sidebar">
         <nav>
           <ul className="sidebar-items">
-            <li className='navlink'>
-              <div><Icon fontSize="medium"><PiPulseBold /></Icon></div>
-              <div><Link to='/dashboard'>Dashboard</Link></div>
-            </li>
-            <li className='navlink'>
-              <div><Icon fontSize="medium"><Inventory2OutlinedIcon /></Icon></div>
-              <div><Link to='/'>Equipamentos</Link></div>
-            </li>
-            <li className='navlink'>
-              <div><Icon fontSize="medium"><Inventory2OutlinedIcon /></Icon></div>
-              <div><Link to='/patrimonios'>Patrimônios</Link></div>
-            </li>
-            <li className='navlink'>
-              <div><Icon fontSize="medium"><Inventory2OutlinedIcon /></Icon></div>
-              <div><Link to='/emprestimos'>Empréstimos</Link></div>
-            </li>
-            <li className='navlink'>
-              <div><Icon fontSize="medium"><SupervisedUserCircleIcon /></Icon></div>
-              <div><Link to='/emprestimos'>Pacientes</Link></div>
-            </li>
-            <li className='navlink'>
-              <div><Icon fontSize='medium'><TrendingUpIcon /></Icon></div>
-              <div><Link>Relatórios</Link></div>
-            </li>
-            <li className='navlink'>
-              <div><Icon fontSize='medium'><SettingsOutlinedIcon /></Icon></div>
-              <div><Link>Configurações</Link></div>
-            </li>
+            {menuItems.map((item) => (
+              <li
+                key={item.path}
+                className={`navlink ${activePath === item.path ? 'selected' : ''}`} // Garantir que a expressão é sempre uma string
+              >
+                <NavLink to={item.path} onClick={() => setActivePath(item.path)}>
+                  <Icon fontSize="medium">{item.icon}</Icon>
+                  <span>{item.label}</span>
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
       <div className="footer-sidebar">
         <div className="profile-block">
           <div className='profile-avatar'>
-            <Icon fontSize='large'><AccountCircleIcon className="profile-icon"/> </Icon>
+            <Icon fontSize='large'><AccountCircleIcon className="profile-icon" /> </Icon>
           </div>
           <div className="profile-info">
-            <div className="profile-name">Usuario</div>
+            <div className="profile-name">Usuário</div>
             <div className="profile-type">Role</div>
           </div>
         </div>
