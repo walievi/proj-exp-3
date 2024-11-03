13ed0cbe7f236236d5f64b16be30920524596408
Vinicius Plack Wecker <viniciuswecker@ienh.com.br>
2024-11-03 02:25:13 -0300

    Ajuste Paginação.

--- frontend/app/src/components/TableList/PaginationUtils.js
+++ frontend/app/src/components/TableList/PaginationUtils.js
@@ -1,31 +1,24 @@
 export const getPaginationPages = (currentPage, totalPages) => {
     let pages = [];
 
-    // Se a página atual for 1, adiciona 1 e as duas páginas seguintes
-    if (currentPage === 1) {
-        pages.push(1);
-        for (let i = 2; i <= Math.min(3, totalPages); i++) {
-            pages.push(i);
-        }
+    // Se a página atual for 1, 2 ou 3, adiciona as primeiras 5 páginas
+    if (currentPage <= 3) {
+        pages.push(1, 2, 3, 4, 5);
     } 
-    // Se a página atual for 2, adiciona 1 e 2
-    else if (currentPage === 2) {
-        pages.push(1, 2);
-    } 
-    // Se a página atual for 3, adiciona 1, 2 e 3
-    else if (currentPage === 3) {
-        pages.push(1, 2, 3);
-    } 
-    // Se a página atual for maior ou igual a 4, adiciona as duas páginas anteriores e a atual
-    else if (currentPage >= 4) {
+    // Se a página atual estiver entre 4 e totalPages - 3
+    else if (currentPage >= 4 && currentPage <= totalPages - 3) {
         pages.push(currentPage - 2); // Número anterior
         pages.push(currentPage - 1); // Segundo número anterior
         pages.push(currentPage);      // Página atual
+        pages.push(currentPage + 1); // Próximo número
+        pages.push(currentPage + 2); // Segundo próximo
     } 
-
-    // Adiciona as próximas duas páginas
-    for (let i = currentPage + 1; i <= Math.min(currentPage + 2, totalPages); i++) {
-        pages.push(i);
+    // Se a página atual estiver nas últimas 3 páginas
+    else {
+        const startPage = Math.max(totalPages - 4, 1); // Para garantir que a primeira página não seja menor que 1
+        for (let i = startPage; i <= totalPages; i++) {
+            pages.push(i);
+        }
     }
 
     // Remove duplicatas e ordena as páginas
